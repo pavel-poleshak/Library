17605ea0c28561c096eff3d95042944948563557
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-01-28 13:55:10 +0300

    BookContext

---
+++ Library/Library/Models/BookContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class BookContext:DbContext
+    {
+        public BookContext() :base ("BookContext")
+        {
+
+        }
+    }
+}
