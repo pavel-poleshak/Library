97936ebe23611cd8391e705a212f9320a23b2293
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-01-28 13:37:48 +0300

    Book Properties

--- Library/Library/Models/Book.cs
+++ Library/Library/Models/Book.cs
@@ -7,5 +7,10 @@ namespace Library.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+
     }
 }
