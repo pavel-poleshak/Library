4c09716c304b50f02c7a135706736938d4c545d9
pavel-poleshak <mr.gorden.beat@gmail.com>
2017-01-29 22:41:30 +0300

    Added Db Initializer

---
+++ Library/Library/Models/BookDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Library.Models
+{
+    public class BookDbInitializer:DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                context.Books.Add(new Book() { Name = "Book " + i, Authors = "Author "+i, PublishingHouse = "Publish House "+i, Description = "Description +i", Price = 100*i });
+            }            
+
+            base.Seed(context);
+        }
+    }
+}
